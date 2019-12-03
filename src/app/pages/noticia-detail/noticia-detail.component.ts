955d842f3a5b2234cc6a2967239a2d87c1da8f55
Jose-arc <joseignaciocacerestrejo@gmail.com>
2019-12-03 17:09:36 -0400

    **funcion para separar img de un string por ',' 03/12/2019**

--- src/app/pages/noticia-detail/noticia-detail.component.ts
+++ src/app/pages/noticia-detail/noticia-detail.component.ts
@@ -1,15 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+
+// Imports
+import { Router, ActivatedRoute, Params } from "@angular/router";
+import { NoticiasService } from "src/app/services/noticias.service";
+import { Noticias } from "src/app/models/noticias.model";
+import { GlobalsService } from "src/app/services/globals.service";
+import Swal from "sweetalert2";
+import { GLOBAL } from "src/app/models/global";
+import { Bancoimg } from "src/app/models/banco.model";
 
 @Component({
-  selector: 'app-noticia-detail',
-  templateUrl: './noticia-detail.component.html',
-  styleUrls: ['./noticia-detail.component.css']
+  selector: "app-noticia-detail",
+  templateUrl: "./noticia-detail.component.html",
+  styleUrls: ["./noticia-detail.component.css"]
 })
 export class NoticiaDetailComponent implements OnInit {
+  public noticia_detail: any[];
+  public imgs: any[];
 
-  constructor() { }
+  constructor(
+    private _g: GlobalsService,
+    private _ns: NoticiasService,
+    private _router: Router,
+    private _route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
+    this.getDetail();
   }
 
+  getDetail() {
+    this._route.params.forEach((params: Params) => {
+      let id = params["id"];
+
+      this._ns.getDetalleNoticia(id).subscribe(
+        resp => {
+          if (resp.code == 200) {
+            this.noticia_detail = resp.data;
+            this.imgs = this._g.separadorImagenes(resp.data.Imagen);
+
+            console.log(this.noticia_detail);
+            console.log(this.imgs);
+          } else {
+            this._router.navigateByUrl("noticias");
+          }
+        },
+        err => {
+          console.log(<any>err);
+        }
+      );
+    });
+  }
 }
--- src/app/services/globals.service.ts
+++ src/app/services/globals.service.ts
@@ -1,88 +1,101 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 
 //Imports
-import { HttpClient,HttpHeaders,HttpParams } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
 // import  'rxjs/add/operator/map';
-import { Observable, from } from 'rxjs';
+import { Observable, from } from "rxjs";
 
 //Global URL
-import { GLOBAL } from '../models/global';
-import { Bancoimg } from '../models/banco.model';
-
-
+import { GLOBAL } from "../models/global";
+import { Bancoimg } from "../models/banco.model";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class GlobalsService {
+  public api_rest: string;
+  public api_rest_full: string;
 
-    public api_rest: string;
-    public api_rest_full: string;
-
-  constructor( public http : HttpClient ) { 
-
+  constructor(public http: HttpClient) {
     this.api_rest = GLOBAL.api_rest;
-
   }
 
-  getFecha(){
-    let date = new Date()
+  getFecha() {
+    let date = new Date();
 
-    let day = date.getDate()
-    let month = date.getMonth() + 1
-    let year = date.getFullYear()
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
 
-    if(month < 10){
-        return `${day}-0${month}-${year}`;
-    }else{
-        return `${day}-${month}-${year}`;
+    if (month < 10) {
+      return `${day}-0${month}-${year}`;
+    } else {
+      return `${day}-${month}-${year}`;
     }
   }
 
-  subirArchivo(url:string, params: Array<string>,files: Array<File>){
-    return new Promise((resolve,reject) =>{
-        var formData: any = new FormData();
-        var xhr = new XMLHttpRequest();
+  subirArchivo(url: string, params: Array<string>, files: Array<File>) {
+    return new Promise((resolve, reject) => {
+      var formData: any = new FormData();
+      var xhr = new XMLHttpRequest();
 
-        for (let i = 0; i < files.length; i++) {
-            
-            formData.append('uploads',files[i], files[i].name);
-            
+      for (let i = 0; i < files.length; i++) {
+        formData.append("uploads", files[i], files[i].name);
+      }
+
+      xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4) {
+          if (xhr.status == 200) {
+            resolve(JSON.parse(xhr.response));
+          } else {
+            reject(xhr.response);
+          }
         }
+      };
 
-        xhr.onreadystatechange = function () {
-            
-            if (xhr.readyState == 4) {
-                if (xhr.status == 200) {
-                    resolve(JSON.parse(xhr.response))
-                }else{
-                    reject(xhr.response);
-                }
-            }
-
-        };
-
-        xhr.open("POST",url,true);
-        xhr.send(formData);
+      xhr.open("POST", url, true);
+      xhr.send(formData);
     });
-}
+  }
 
-addbanco(banco: Bancoimg): Observable<any>{
+  addbanco(banco: Bancoimg): Observable<any> {
     let json = JSON.stringify(banco);
-    let params = "banco="+json;
-    let headers = new HttpHeaders().set('Content-Type','application/x-www-form-urlencoded');
-     
-    return this.http.post(this.api_rest+'banco', params, {headers: headers});
-  }
-  
-  getBanco():Observable<any>{
-    return this.http.get(this.api_rest+'banco');
-  }
-  
-  getBancoDetalle(id):Observable<any>{
-    return this.http.get(this.api_rest+'banco/'+id);
+    let params = "banco=" + json;
+    let headers = new HttpHeaders().set(
+      "Content-Type",
+      "application/x-www-form-urlencoded"
+    );
+
+    return this.http.post(this.api_rest + "banco", params, {
+      headers: headers
+    });
   }
 
+  getBanco(): Observable<any> {
+    return this.http.get(this.api_rest + "banco");
+  }
+
+  getBancoDetalle(id): Observable<any> {
+    return this.http.get(this.api_rest + "banco/" + id);
+  }
+
+  separadorImagenes(s) {
+    let images = [];
+    let cadena = s;
+    let caracter = ",";
+    let inicio = 0;
+    let termino = cadena.length;
+
+    for (let i = 0; i < cadena.length; i++) {
+      if (cadena.charAt(i) == caracter) {
+        // console.log("Numero donde esta la , :" + i);
+        images.push(cadena.substring(inicio, i));
+        inicio = i + 1;
+      }
+    }
+
+    images.push(cadena.substring(inicio, termino));
+
+    return images;
+  }
 }
-
-
