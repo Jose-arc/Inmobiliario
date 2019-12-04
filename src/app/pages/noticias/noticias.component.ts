911aa175967bce8896765e2c2feff0bcffe5f356
Jose-arc <joseignaciocacerestrejo@gmail.com>
2019-12-04 11:30:37 -0400

    **funcion para eliminar entradas(Noticias) 04/12/2019**

--- src/app/pages/noticias/noticias.component.ts
+++ src/app/pages/noticias/noticias.component.ts
@@ -1,52 +1,107 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 // Imports
-import { Router } from '@angular/router';
-import { NoticiasService } from 'src/app/services/noticias.service';
-import { Noticias } from 'src/app/models/noticias.model';
-import { GlobalsService } from 'src/app/services/globals.service';
-import Swal from 'sweetalert2';
-import { GLOBAL } from 'src/app/models/global';
-import { Bancoimg } from 'src/app/models/banco.model';
+import { Router } from "@angular/router";
+import { NoticiasService } from "src/app/services/noticias.service";
+import { Noticias } from "src/app/models/noticias.model";
+import { GlobalsService } from "src/app/services/globals.service";
+import Swal from "sweetalert2";
+import { GLOBAL } from "src/app/models/global";
+import { Bancoimg } from "src/app/models/banco.model";
 
 @Component({
-  selector: 'app-noticias',
-  templateUrl: './noticias.component.html',
-  styleUrls: ['./noticias.component.css'],
+  selector: "app-noticias",
+  templateUrl: "./noticias.component.html",
+  styleUrls: ["./noticias.component.css"],
   providers: [NoticiasService]
 })
 export class NoticiasComponent implements OnInit {
+  public titulo: string;
+  public noticias: any[];
 
-  public titulo : string;
-  public noticias : any[];
-
-  constructor(private _ns: NoticiasService,
-              private _g: GlobalsService,
-              private _router: Router) { 
-
-    this.titulo = 'Noticias';
-
+  constructor(
+    private _ns: NoticiasService,
+    private _g: GlobalsService,
+    private _router: Router
+  ) {
+    this.titulo = "Noticias";
   }
 
   ngOnInit() {
-
     this.getNoticias();
-
   }
 
-  getNoticias(){
-    this._ns.getNoticias().subscribe(
-      result =>{
-        if (result.code != 200) {
-          console.log(result);
-        }else{
+  getNoticias() {
+    this._ns.getNoticias().subscribe(result => {
+      if (result.code != 200) {
+        console.log(result);
+      } else {
+        this.noticias = result.mensaje;
+        console.log(result.mensaje);
+      }
+    });
+  }
 
-          this.noticias = result.mensaje;
-          console.log(result.mensaje);
+  borrarConfirmado(id) {
+    const swalWithBootstrapButtons = Swal.mixin({
+      customClass: {
+        confirmButton: "btn btn-sm btn-success",
+        cancelButton: "btn btn-sm btn-danger"
+      },
+      buttonsStyling: false
+    });
 
+    swalWithBootstrapButtons
+      .fire({
+        title: "Estas seguro?",
+        text: "Esta entrada se eliminara permanentemente de la Base de datos",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonText: "Si, Borrar",
+        cancelButtonText: "No, Cancelar",
+        reverseButtons: true
+      })
+      .then(result => {
+        if (result.value) {
+          this.deleteNoticia(id);
+
+          swalWithBootstrapButtons.fire(
+            "Eliminado!",
+            "Tu entrada fue removida exitosamente.",
+            "success"
+          );
+        } else if (result.dismiss === Swal.DismissReason.cancel) {
+          swalWithBootstrapButtons.fire(
+            "Cancelado",
+            "Sabia decisión el archivo quedo a salvo",
+            "error"
+          );
         }
+      });
+  }
+
+  deleteNoticia(id) {
+    this._ns.deleteNoticia(id).subscribe(
+      resp => {
+        if (resp.code == 200) {
+          this.getNoticias();
+        } else {
+          Swal.fire({
+            allowOutsideClick: false,
+            icon: "error",
+            title: "Error",
+            text: resp.mensaje
+          });
+        }
+      },
+      err => {
+        Swal.fire({
+          allowOutsideClick: false,
+          icon: "error",
+          title: "Error",
+          text: err
+        });
       }
     );
   }
-
 }
